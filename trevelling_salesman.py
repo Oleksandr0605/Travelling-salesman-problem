daec55a1364768ba3d1bc121d83ad2a2c134e1fc
Oleksandr Ivaniuk <ivaniuk.pn@ucu.edu.ua>
2022-12-09 17:45:01 +0200

    first

---
+++ trevelling_salesman.py
@@ -0,0 +1,23 @@
+def read_file():
+    """
+    """
+    pass
+
+def greedy():
+    """
+    """
+    pass
+
+def exact():
+    """
+    """
+    pass
+
+def main():
+    """
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
